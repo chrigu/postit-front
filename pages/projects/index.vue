068392c110efd32acbdab09aee76ff4e6851e667
Chrigu <christian.cueni@iterativ.ch>
2018-10-21 22:07:16 +0200

    Add basic form

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="example-simple">
     <h1 id="example-title" class="example-title">Projects</h1>
+    <section class="form">
+      <div class="field">
+        <label class="label">Project name</label>
+        <div class="control">
+          <input v-model="form.name" v-validate="'required|min:3'" class="input" type="text" placeholder="Name">
+          <p class="help is-danger" v-show="errors.has('name')">
+            {{ errors.first('name') }}
+          </p>
+        </div>
+      </div>
+    </section>
       <button type="button" class="btn btn-danger">
           <i class="fa fa-stop" aria-hidden="true"></i>
           New Project
@@ -21,8 +32,9 @@ export default {
   components: {},
   data() {
     return {
-      files: [],
-      status: ''
+      form: {
+        name: ''
+      }
     }
   },
 
